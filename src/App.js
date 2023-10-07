8557a867af1e193a9ddce613012fa8bf334add44
mike-charity <Mikecharity0@gmail.com>
2023-10-07 09:15:15 -0400

    link

--- src/App.js
+++ src/App.js
@@ -9,10 +9,8 @@ export default function App() {
       <footer>
         This project was coded by{" "}
         <a href="https://www.shecodes.io/" target="_blank">
-          charity
+          charity and is
         </a>
-        {""}
-        and is{" "}
         <a href="https://github.com/mike-charity/reactweather" target="_blank">
           open-sourced on Github
         </a>
