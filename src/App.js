63b9f5611a6989619ae7a9ae021ce147513548a6
mike-charity <Mikecharity0@gmail.com>
2023-10-07 08:38:43 -0400

    link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React from "react ";
-export default function App(){
-  return(<div className="App">
-    <h1>Weather App</h1>
-    <footer>This project was coded by 
-      {" "}
-      <a href="https://www.shecodes.io/" target="_blank">
-        charity
-      </a>{" "}
-      and is{" "}
-      <a href=""
-    </footer>
-  </div>
+import React from "react";
+import "./App.css";
 
-  )
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      
+    </div>
+  );
 }
