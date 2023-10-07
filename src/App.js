b598de268f6b75aa240a456646ac66857b8b77b1
mike-charity <Mikecharity0@gmail.com>
2023-10-07 08:49:34 -0400

    updated app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      
+      <Weather />
     </div>
   );
 }
