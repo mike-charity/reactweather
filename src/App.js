94971c947fe9ae5dcdb98e64b0cd83fd20e85e69
mike-charity <Mikecharity0@gmail.com>
2023-10-08 08:54:06 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import "./App.css";
-import Weather from "./Weather";
+
+
+
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
-      <Weather />
-     
+      
+
+      <footer>
+        This project was coded by
+        <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+          charity and is
+        </a>
+        <p>
+          <a
+            href="https://github.com/mike-charity/reactweather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </p>
+      </footer>
+    </div>
     </div>
   );
 }
