3909975dea2c92a943550cf919f36feade01d7db
mike-charity <Mikecharity0@gmail.com>
2023-10-09 15:48:25 -0400

    updated index.js

--- src/App.js
+++ src/App.js
@@ -9,21 +9,6 @@ export default function App() {
         <h1>Weather App</h1>
 
         <Weather defaultcity="Guelph" />
-        <footer>
-          This project was coded by {""}
-          <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
-            charity and is
-          </a>
-          {""}
-          and is {""}
-          <a
-            href="https://github.com/mike-charity/reactweather.git"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on Github
-          </a>
-        </footer>
       </div>
     </div>
   );
