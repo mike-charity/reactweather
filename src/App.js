e0beda7359c00e26d0f82f2eb78580df61fe8945
mike-charity <Mikecharity0@gmail.com>
2023-10-17 18:38:47 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Weather from "./Weather";
 import "./App.css";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <div className="container">
@@ -24,3 +24,5 @@ export default function App() {
     </div>
   );
 }
+
+export default App;
